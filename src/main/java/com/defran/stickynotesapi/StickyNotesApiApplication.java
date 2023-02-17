3a2455dd9685513be66579f3fc15164904078bc0
paulsbunion <defrain.3@gmail.com>
2023-02-16 19:46:06 -0500

    initial setup

---
+++ src/main/java/com/defran/stickynotesapi/StickyNotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.defran.stickynotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickyNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickyNotesApiApplication.class, args);
+	}
+
+}
